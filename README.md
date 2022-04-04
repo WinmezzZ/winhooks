ffe7c6eac6c8779d3083dd995eadd8b501aa1eef
Winme <winme0308@gmail.com>
2022-04-04 23:27:16 +0800

    docs: add github link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-hooks
+
 Some useful react hooks collections.
+
+Docs: https://react-hooks.hyyar.com/
--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,11 @@ function App() {
             <NavLink to="/api">Api</NavLink>
           </li>
         </ul>
+        <div className="right-icon">
+          <a href="https://github.com/WinmezzZ/react-hooks" target="_blank" rel="noopener noreferrer">
+            Github
+          </a>
+        </div>
       </header>
       <section className="app-section">
         <Suspense fallback={null}>
