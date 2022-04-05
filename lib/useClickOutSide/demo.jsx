84561d4effe30bb188cdc222648515767dac429c
Winme <winme0308@gmail.com>
2022-04-06 03:18:11 +0800

    docs: useClickOutSide doc demo update

--- lib/useClickOutSide/demo.jsx
+++ lib/useClickOutSide/demo.jsx
@@ -1,18 +1,18 @@
 import { useClickOutSide } from '@winme/react-hooks';
 import { useRef } from 'react';
-import { Button } from 'ultra-design';
+import { Button, Toast } from 'ultra-design';
 
 export default () => {
   const ref = useRef(null);
 
   useClickOutSide(ref, () => {
-    console.log('Clicked');
+    Toast.info('Clicked');
   });
 
   return (
     <div className="demo-wrapper">
       <Button type="primary" ref={ref}>
-        Click outside to console log
+        Click outside to show alert, not this button!
       </Button>
     </div>
   );
