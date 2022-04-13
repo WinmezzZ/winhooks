32d256c9df0040ce9fc7b163d2557c3dc362a904
Winme <winme0308@gmail.com>
2022-04-13 11:03:43 +0800

    docs: 修改 header 菜单样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React, { Suspense } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Menu } from 'ultra-design';
 import './App.less';
 import RenderRouter from './routes';
+import { getFirstPathCode } from './utils/getFirstPathName';
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onMenuClick = (key: string) => {
+    if (!key) return;
+    navigate(key);
+  };
+
   return (
     <div className="App">
       <header className="app-header">
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/guide">Guide</NavLink>
-          </li>
-          <li>
-            <NavLink to="/api">Api</NavLink>
-          </li>
-        </ul>
-        <div className="right-icon">
-          <a href="https://github.com/WinmezzZ/react-hooks" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-        </div>
+        <Menu horizontal defaultSelectedKey={'/' + getFirstPathCode(location.pathname)} onClick={onMenuClick}>
+          <Menu.SubMenu key="/">Home</Menu.SubMenu>
+          <Menu.SubMenu key="/guide">Guide</Menu.SubMenu>
+          <Menu.SubMenu key="/api">Api</Menu.SubMenu>
+          <Menu.SubMenu key="">
+            <a href="https://github.com/WinmezzZ/react-hooks" target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+          </Menu.SubMenu>
+        </Menu>
       </header>
       <section className="app-section">
         <Suspense fallback={null}>
